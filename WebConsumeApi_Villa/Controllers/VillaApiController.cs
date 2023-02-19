79f15f8fdb2a0fd18ef627baf9716df5b73a94e3
samirkhan-947 <samirrkhan947@gmail.com>
2023-02-19 21:22:07 +0530

    First EndPoint API

---
+++ WebConsumeApi_Villa/Controllers/VillaApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebConsumeApi_Villa.Models;
+
+namespace WebConsumeApi_Villa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id=1,Name="salamu"},
+                new Villa {Id=2,Name="saif"},
+                new Villa {Id=3,Name="shahrukh"},
+               
+            };
+        }
+    }
+}
---
+++ WebConsumeApi_Villa/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace WebConsumeApi_Villa.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
