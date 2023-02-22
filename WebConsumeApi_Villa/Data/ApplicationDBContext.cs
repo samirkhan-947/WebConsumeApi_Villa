004e9b753e7ab0396b909238684c24388cfe2b95
samirkhan-947 <samirrkhan947@gmail.com>
2023-02-22 12:10:52 +0530

    implmented with seeding

--- WebConsumeApi_Villa/Data/ApplicationDBContext.cs
+++ WebConsumeApi_Villa/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
 using WebConsumeApi_Villa.Models;
 
 namespace WebConsumeApi_Villa.Data
@@ -8,6 +9,48 @@ namespace WebConsumeApi_Villa.Data
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(                
+                new Villa
+                {
+                    Id= 1,
+                    Name="Banglore villa",
+                    Details ="this is the banglaure villa with beautyful house and decent price ",
+                    ImageUrl ="",
+                    Occupancy= 5,
+                    Rate= 200,
+                    Sqft= 550,
+                    Amenity="",
+                    CreateDate= DateTime.Now,
+                },
+                 new Villa
+                 {
+                     Id = 2,
+                     Name = "pune villa",
+                     Details = "this is the pune villa with beautyful house and decent price ",
+                     ImageUrl = "",
+                     Occupancy = 4,
+                     Rate = 300,
+                     Sqft = 450,
+                     Amenity = "",
+                     CreateDate = DateTime.Now,
+                 },
+                 new Villa
+                 {
+                     Id = 3,
+                     Name = "patna villa",
+                     Details = "this is the pune villa with beautyful house and decent price ",
+                     ImageUrl = "",
+                     Occupancy = 3,
+                     Rate = 600,
+                     Sqft = 850,
+                     Amenity = "",
+                     CreateDate = DateTime.Now,
+                 }
+            );
         }
         public DbSet<Villa> villas { get; set; }
     }
---
+++ WebConsumeApi_Villa/Migrations/20230222063914_seedingwithdata.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebConsumeApi_Villa.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingwithdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "villas",
+                columns: new[] { "Id", "Amenity", "CreateDate", "Details", "ImageUrl", "Name", "Occupancy", "Rate", "Sqft", "UpdateDate" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(2023, 2, 22, 12, 9, 14, 488, DateTimeKind.Local).AddTicks(8591), "this is the banglaure villa with beautyful house and decent price ", "", "Banglore villa", 5, 200.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "", new DateTime(2023, 2, 22, 12, 9, 14, 488, DateTimeKind.Local).AddTicks(8602), "this is the pune villa with beautyful house and decent price ", "", "pune villa", 4, 300.0, 450, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "", new DateTime(2023, 2, 22, 12, 9, 14, 488, DateTimeKind.Local).AddTicks(8605), "this is the pune villa with beautyful house and decent price ", "", "patna villa", 3, 600.0, 850, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "villas",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
